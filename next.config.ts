ede82b0342e550968a6a313275fe968c47037928
lucaluuteo <lucaluuteo@gmail.com>
2025-03-29 12:57:26 +0800

    Fix lỗi hiển thị hình ảnh, thay thế bằng thẻ <Image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'happygut.vercel.app', 'raw.githubusercontent.com'], // Thêm các domain bạn cần
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
